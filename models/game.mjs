1a28acd3de63fbbfa71995f1d8fe3e261d6b3415
wzjustuslim <weizhengjustus.lim@gmail.com>
2021-12-27 22:36:01 +0800

    db models

---
+++ models/game.mjs
@@ -0,0 +1,23 @@
+export default function initGameModel(sequelize, DataTypes) {
+  return sequelize.define('game', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+    },
+    gameState: {
+      type: DataTypes.JSON,
+    },
+    createdAt: {
+      allowNull: false,
+      type: DataTypes.DATE,
+    },
+    updatedAt: {
+      allowNull: false,
+      type: DataTypes.DATE,
+    },
+  }, { 
+    underscored: true, 
+  });
+}
---
+++ models/user.mjs
@@ -0,0 +1,28 @@
+export default function initUserModel(sequelize, DataTypes) {
+  return sequelize.define('user', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+    },
+    email: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+    password: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+    createdAt: {
+      allowNull: false,
+      type: DataTypes.DATE,
+    },
+    updatedAt: {
+      allowNull: false,
+      type: DataTypes.DATE,
+    },
+  }, { 
+    underscored: true, 
+  });
+}
